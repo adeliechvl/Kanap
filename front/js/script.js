f6cbd52306445799c8ff112c1b7104320ecd99af
Adélie Chauvel <adelie.chauvel@yahoo.fr>
2022-11-08 21:54:24 +0100

    insérer produits dans index

--- front/js/script.js
+++ front/js/script.js
@@ -1,12 +1,53 @@
-function request() {
-    fetch("http://localhost:3000/api/products")
-    .then(function(res) {
-        if (res.ok) {
-          return res.json();
-        }
-      })
-    }
+//DOM
+const items = document.querySelector("#items");
 
-    fetch("http://localhost:3000/api/products")
-  .then((res) => res.json())
-  .then
+//fetch api
+async function getProducts() {
+  try {
+    const response = await fetch("http://localhost:3000/api/products");
+    const data = await response.json();
+    displayProducts(data);
+  } 
+  
+  catch (e) {
+    console.log(e);
+  }
+}
+
+getProducts();
+
+//create and display items in index html
+function displayProducts(data) {
+
+  //loop through data's array
+  for (const i of data) {
+
+    //create elements pour each array index
+    const link = document.createElement("a");
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+
+    //assign elements
+    link.href = `./product.html?id=${i._id}`;
+    img.src = i.imageUrl;
+    img.alt = i.altTxt;
+    h3.textContent = i.name;
+    p.textContent = i.description;
+
+    //appended child layout
+    items.appendChild(link);
+    link.appendChild(article);
+    article.appendChild(img);
+    article.appendChild(h3);
+    article.appendChild(p);
+  }
+}
+
+function showError() {
+  const articleError = document.createElement("article");
+  articleError.innerHTML = "Aucun Article disponible, réessayez plus tard";
+  const parent = document.getElementById("#items");
+  parent.appendChild(articleError);
+}
